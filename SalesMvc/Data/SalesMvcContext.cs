b2377309aee5c96527ebf4f16b543851ab4c0ec7
ricand7 <ricantonioj@hotmail.com>
2022-07-26 11:32:00 -0300

    initial commit

---
+++ SalesMvc/Data/SalesMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMvc.Models.ViewModels;
+
+namespace SalesMvc.Data
+{
+    public class SalesMvcContext : DbContext
+    {
+        public SalesMvcContext (DbContextOptions<SalesMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesMvc.Models.ViewModels.Department> Department { get; set; }
+    }
+}
