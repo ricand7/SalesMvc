18e7c4ee49b3513792bd22b1b4e4f5388e92e89c
ricand7 <ricantonioj@hotmail.com>
2022-07-28 16:11:36 -0300

    feat add sellerformviewmodel

---
+++ SalesMvc/Services/DepartmentService.cs
@@ -0,0 +1,27 @@
+﻿using SalesMvc.Data;
+using SalesMvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesMvc.Services
+{
+    public class DepartmentService
+    {
+
+        private readonly SalesMvcContext _context;
+        public DepartmentService(SalesMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+
+
+    }
+}
