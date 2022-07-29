e8a862e388d4d62243898076f37c952087f7218f
ricand7 <ricantonioj@hotmail.com>
2022-07-29 06:02:45 -0300

    Update seller and add Service Exception

--- SalesMvc/Controllers/SellersController.cs
+++ SalesMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using SalesMvc.Models.ViewModels;
 using System.Threading.Tasks;
+using SalesMvc.Services.Exceptions;
 
 namespace SalesMvc.Controllers
 {
@@ -85,6 +86,51 @@ namespace SalesMvc.Controllers
 
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+
+
+
+        }
 
     }
 }
--- SalesMvc/Services/SellerService.cs
+++ SalesMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesMvc.Services.Exceptions;
 
 namespace SalesMvc.Services
 {
@@ -43,6 +44,23 @@ namespace SalesMvc.Services
             _context.SaveChanges();
         }
 
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+          
+        }
 
 
 
